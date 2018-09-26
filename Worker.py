626d5939d2adc4fa4657c2b6b5a644784f5611fe
Hardy-c <corentin.hardy@irisa.fr>
2018-09-26 11:44:16 +0200

    Update copyright

--- Worker.py
+++ Worker.py
@@ -1,15 +1,20 @@
 '''Copyright (c) 2015 – Thomson Licensing, SAS
+
 Redistribution and use in source and binary forms, with or without
 modification, are permitted (subject to the limitations in the
 disclaimer below) provided that the following conditions are met:
+
 * Redistributions of source code must retain the above copyright
 notice, this list of conditions and the following disclaimer.
+
 * Redistributions in binary form must reproduce the above copyright
 notice, this list of conditions and the following disclaimer in the
 documentation and/or other materials provided with the distribution.
+
 * Neither the name of Thomson Licensing, or Technicolor, nor the names
 of its contributors may be used to endorse or promote products derived
 from this software without specific prior written permission.
+
 NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
 GRANTED BY THIS LICENSE.  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
 HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
@@ -22,7 +27,8 @@ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
 BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
 WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.'''
+IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+'''
 
 import tensorflow as tf
 import Tf_op as df
